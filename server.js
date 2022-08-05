fa011c37e5c34decb072ec58b7ebe83f10cd2a88
Leonardo <d3pro3@outlook.com>
2022-08-05 10:51:06 -0300

    teste agora vai 2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/meu-app-heroku-teste'));
+app.use(express.static('./dist/heroku-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/meu-app-heroku-teste/'}),
+    res.sendFile('index.html', {root: 'dist/heroku-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
